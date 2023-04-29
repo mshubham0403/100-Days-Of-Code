363cf22be25d74cebb1ebcebe6e6083195f4bd40
mshubham0403 <75219721+mshubham0403@users.noreply.github.com>
2023-04-30 02:38:56 +0530

    day-7

--- Day-6_(13)_RomanToInt.cpp
+++ Day-6_(13)_RomanToInt.cpp
@@ -2,7 +2,43 @@
 #include<stdio.h>
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
 
+
+
+
+
+int romanToInt(string s) {
+        unordered_map<char, int> romToInt = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        char prev = 'M';
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ii = s[i];
+            if (romToInt[prev] < romToInt[ii]) {
+                result -= 2 * romToInt[prev];
+                result += romToInt[ii];
+                prev = ii;
+            } else {
+                result += romToInt[ii];
+                prev = ii;
+            }
+        }
+
+        return result;
+    }
+
+int main(){
+string s ="MCMXVII";
+int ans = romanToInt(s);
+cout<<ans<<endl;
 return 0;
 }
