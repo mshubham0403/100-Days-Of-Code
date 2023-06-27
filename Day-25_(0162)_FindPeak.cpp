25355940e7b1cbf2e9a30b290671da92fa774e44
mshubham0403 <75219721+mshubham0403@users.noreply.github.com>
2023-06-27 23:25:03 +0530

    day-26

--- Day-25_(0162)_FindPeak.cpp
+++ Day-25_(0162)_FindPeak.cpp
@@ -1,44 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-   int findPeakElement(vector<int>& nums) {
-   int n = nums.size();
-      if(n==1) return 0;
-      if(nums[n-1]>nums[n-2]) return n-1;
-      if(nums[0]>nums[1]) return 0;
-      int low=1,high=n-2;
+   int singleNonDuplicate(vector<int>& nums) {
+   int n=nums.size();
+    if(n==1)return nums[0];
+    if(nums[0]!=nums[1]) return nums[0];
+    if(nums[n-1]!=nums[n-2]) return nums[n-1];
+      int  low=1,high=n-2;
       while(low<=high){
-        int  mid =low+((high-low)/2);
-          if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
-              return mid;
+          int mid=low+((high-low)/2);
+          
+                if(mid%2==0){
+                    if(nums[mid+1]!=nums[mid] && nums[mid-1]!=nums[mid]){
+                        return nums[mid];
+                    }
+                    else if(nums[mid+1]==nums[mid]){
+                        low=mid+1;
+                    }
+                    else{
+                        high=mid-1;
+                    }
+                }
+              else{
+                  if(nums[mid+1]!=nums[mid] && nums[mid-1]!=nums[mid]){
+                        return nums[mid];
+                    }
+                    else if(nums[mid+1]!=nums[mid]){
+                        low=mid+1;
+                    }
+                    else{
+                        high=mid-1;
+                    }
+              }
           }
-          else if(nums[mid+1]>nums[mid]){
-              high=mid-1;
-          }
-          else{
-              low=mid+1;
-          }
-      }
-      low=1;high=n-2;
-      while(low<=high){
-        int  mid =low+((high-low)/2);
-          if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
-              return mid;
-          }
-          else if(nums[mid+1]<nums[mid]){
-              high=mid-1;
-          }
-          else{
-              low=mid+1;
-          }
-      }
-      return 0;
+        
+       
       
+      return nums[0];
   }
 int main(){
-vector<int>nums={2,3,4,12,15,17,29,23,24,22,19,11,10};
+vector<int>nums={34,34,56,56,12,12,13,14,14,15,15,17,17};
 
-int ans = findPeakElement(nums);
+int ans = singleNonDuplicate(nums);
 cout<<ans<<endl;
 
 return 0;
