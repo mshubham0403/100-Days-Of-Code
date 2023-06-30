d25a97484340e6d69a38e80d5dde2e369389ae8a
mshubham0403 <75219721+mshubham0403@users.noreply.github.com>
2023-06-30 14:21:49 +0530

    day-32

--- Day-31_(0136)_SingleNumber.cpp
+++ Day-31_(0136)_SingleNumber.cpp
@@ -2,18 +2,32 @@
 using namespace std;
 
   
-    int singleNumber(vector<int>& nums) {
-        int res = 0;
-        for(int num : nums) {
-            res ^= num;
+   vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> m1;
+        pair<int,int> p1;
+        vector<int> ans;
+        for (int i=0;i<nums.size();i++){
+            m1.emplace(nums[i],i);
         }
-        return res;
+        for (int j=0;j<nums.size();j++){
+            int chk=target-nums[j];
+            if(m1.find(chk)!=m1.end() && m1[chk]!=j){
+                p1.first = j;
+                p1.second = m1[chk];
+            }
+            
+        }
+        ans.emplace_back(p1.first);
+        ans.emplace_back(p1.second);
+
+        return ans;
+
     }
 int main(){
-vector<int>nums={11,11,2,3,3,4,4,55,55};
+vector<int>nums={1,2,3,7,4,4,9,5};
 
- singleNumber(nums);
-for(auto &x:nums){
+ vector<int> ans =twoSum(nums,8);
+for(auto &x:ans){
   
         cout<<x<<" ";
     }
