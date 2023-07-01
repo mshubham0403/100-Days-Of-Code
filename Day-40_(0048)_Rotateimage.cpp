38a6a3553fd24ee938db0f006b7a08bfebce0aac
mshubham0403 <75219721+mshubham0403@users.noreply.github.com>
2023-07-01 19:53:38 +0530

    day-42

--- Day-40_(0048)_Rotateimage.cpp
+++ Day-40_(0048)_Rotateimage.cpp
@@ -1,42 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> spiralOrder(vector<vector<int>>& matrix) {
-    if (matrix.empty())
-      return {};
-
-    const int m = matrix.size();
-    const int n = matrix[0].size();
-    vector<int> ans;
-    int br = 0;
-    int bc = 0;
-    int er = m - 1;
-    int ec = n - 1;
-
-   
-    while (br<=er && bc<=ec) {
-      for (int j = bc; j <= ec && ans.size() < m * n; j++)
-        ans.emplace_back(matrix[br][j]);
-      for (int i = br + 1; i <= er - 1 && ans.size() < m * n; i++)
-        ans.emplace_back(matrix[i][ec]);
-      for (int j = ec; j >=bc  && ans.size() < m * n; j--)
-        ans.emplace_back(matrix[er][j]);
-      for (int i = er- 1; i >= br + 1 && ans.size() < m * n; i--)
-        ans.emplace_back(matrix[i][bc]);
-      br++, bc++, er--, ec--;
-    }
+ int subarraySum(vector<int>& nums, int k) {
+    int ans = 0;
+   unordered_map<int,int>sumH;
+      int N=nums.size();
+      long long sumCurr;
+      sumH[0]=1;
+      for(int i=0;i<N;i++){
+          sumCurr+=nums[i];
+          ans+=sumH[sumCurr-k];
+          sumH[sumCurr]++;
+          
+      }
 
     return ans;
   }
 int main(){
-vector<vector<int>> nums={{1,2,3},{8,99,45},{0,21,0}};
+vector<int> nums={1,2,4,4,46,121,12,33,33,43,12,0,1,12,12,13,14,15,12};
 
- vector<int> ans = spiralOrder(nums);
-for(auto &x:ans){
+ int ans = subarraySum(nums);
+// for(auto &x:ans){
    
-        cout<<x<<" ";
+//         cout<<x<<" ";
     
-}
+// }
+cout<<ans; 
 cout<<endl;
 
 
