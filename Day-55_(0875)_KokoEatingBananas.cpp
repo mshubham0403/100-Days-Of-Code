3504e466809d18926943e3fde6fa28ed8471e3d2
mshubham0403 <75219721+mshubham0403@users.noreply.github.com>
2023-07-04 21:25:41 +0530

    day-55

--- Day-55_(0875)_KokoEatingBananas.cpp
+++ Day-55_(0875)_KokoEatingBananas.cpp
@@ -1,47 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
-  //MS001
+   //day-55
     
 
-  int findPeakElement(vector<int>& nums) {
-   int n = nums.size();
-      if(n==1) return 0;
-      if(nums[n-1]>nums[n-2]) return n-1;
-      if(nums[0]>nums[1]) return 0;
-      int low=1,high=n-2;
+     //MS001
+  int minEatingSpeed(vector<int>& piles, int h) {
+    int n=piles.size();
+      int maxP=0;
+      long long mink=INT_MAX;
+   
+      for(auto&i :piles){
+          maxP=max(maxP,i);
+      }
+      int low=1;
+      int high=2*maxP,best=h;;
       while(low<=high){
-        int  mid =low+((high-low)/2);
-          if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
-              return mid;
+          long long mid=low+((high-low)/2);
+          long long n=0;
+          
+          for(auto &i:piles){
+              n+=i%mid?(i/mid)+1:i/mid;
           }
-          else if(nums[mid+1]>nums[mid]){
-              high=mid-1;
-          }
-          else{
+          if(n>h){
+          
+             
+              cout<<"ans n>h"<<mink<<" n "<<n<<endl;
               low=mid+1;
           }
-      }
-      low=1;high=n-2;
-      while(low<=high){
-        int  mid =low+((high-low)/2);
-          if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
-              return mid;
-          }
-          else if(nums[mid+1]<nums[mid]){
+          else{
+             
+           
+              mink=min(mink,mid);
               high=mid-1;
           }
-          else{
-              low=mid+1;
-          }
+          
       }
-      return 0;
-      
+      return mink;
   }
 int main(){
 
-vector<int>nums= {91,102,5,6,23,12,87};
+vector<int>nums= {91,102,5,6,23,12,87,43,12,87,98,11};
 
-int ans  =findPeakElement(nums);
+int ans  =minEatingSpeed(nums,18);
 cout<<ans;
  cout<<endl;
 
