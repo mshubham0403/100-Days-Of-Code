0c780e62c1358afe0ebc235f065d0970384aba59
mshubham0403 <75219721+mshubham0403@users.noreply.github.com>
2023-07-16 00:28:21 +0530

    day-83

---
+++ Day-83_(0237)_DeleteNodeInLinkedList.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+   //day-70
+   //MS001;
+
+
+ class Node{
+    public:
+    int val;
+    Node* next;
+
+
+    Node(int x){
+        val=x;
+    }
+    void addNext(int x){
+        Node* nextP = new Node(x);
+        this ->next =nextP;
+    }
+ 
+     
+
+ };
+    void printList(Node* head){
+        Node* itr =head;
+        while(itr!=nullptr){
+            cout<<itr->val<<"->";
+        }
+        cout<<"N0\n";
+    }
+ Node* makeList(vector<int>nums){
+    Node* head =new Node(nums[0]);
+    Node*itr =head;
+    for(int i=1;i<nums.size();i++){
+    itr->addNext(nums[i]);
+    itr=itr->next;
+    }
+    itr->next=nullptr;
+    return head;
+
+ }  
+  void deleteNode(Node* head) {
+ 
+   
+    head->val=head->next->val;
+    head->next =head->next->next;
+    }
+
+int main(){
+
+int n;
+
+vector<int>Nodes= {2,13,5,67,54,-12,33,15,68,12,24,-14,-15,-61,99,88,64};
+
+Node* head =makeList(Nodes);
+Node* delNode = head->next->next->next;
+printList(head);
+deleteNode(head);
+printList(head);
+return 0;
+}
