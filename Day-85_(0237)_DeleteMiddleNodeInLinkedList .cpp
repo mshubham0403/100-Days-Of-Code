f5e37bac4fecc4b05a6c7710d30807333e5c3624
mshubham0403 <75219721+mshubham0403@users.noreply.github.com>
2023-07-17 00:42:39 +0530

    Day-85

--- Day-85_(0237)_DeleteMiddleNodeInLinkedList .cpp
+++ Day-85_(0237)_DeleteMiddleNodeInLinkedList .cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-   //day-84
+   //day-83
    //MS001;
 
 
@@ -39,31 +39,11 @@ using namespace std;
     return head;
 
  }  
-   Node* deleteMiddle(Node* head) {
-        if(head==nullptr ) return head;
-        if(head->next==nullptr){
-            head=nullptr;
-            return head;
-        }
-        int size=0;
-        Node * ptr =head;
-        while(ptr!=nullptr){
-            size++;
-            ptr=ptr->next;
-        }
-        int middle =size/2;
-        int cnt=0;
-        ptr=head;
-        while(cnt<middle-1){
-            cnt++;
-            ptr=ptr->next;
-        }
-        Node* a=ptr->next;
-        ptr->next =ptr->next->next;
-        a->next=nullptr;
-        return head;
-        
-        
+  void deleteNode(Node* head) {
+ 
+   
+    head->val=head->next->val;
+    head->next =head->next->next;
     }
 
 int main(){
@@ -75,7 +55,9 @@ vector<int>Nodes= {2,13,5,67,54,-12,33,15,68,12,24,-14,-15,-61,99,88,64};
 Node* head =makeList(Nodes);
 Node* delNode = head->next->next->next;
 printList(head);
-Node* ans =deleteMiddle(head);
-printList(ans);
+deleteNode(head);
+printList(head);
+
+
 return 0;
 }
